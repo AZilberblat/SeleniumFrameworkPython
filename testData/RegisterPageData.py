ed8794d703e7aa5b5bb60ec4a9cef70552c2c452
Abraham Zilberblat <abraham.zilberblat@QUALITEST.LOCAL>
2021-02-16 14:10:47 +0200

    added test data using xls

--- testData/RegisterPageData.py
+++ testData/RegisterPageData.py
@@ -1,8 +1,18 @@
+import pandas as pd
 class RegisterPageData:
 
     test_register_data = [{"firstName": "Abraham", "lastName": "Gilbert", "email": "testing457dhg54@gmail.com", "password": "abcd1234"}]
 
     outputs = {"success": "כיף שהצטרפתם אלינו! החיים הטובים מתחילים כאן"}
+    @staticmethod
+    def getTestData():
+        dfs = pd.read_excel(r"C:\Users\abraham.zilberblat\PycharmProjects\SeleniumFramework\testData\registerData.xlsx")
+        listOfData = []
+        for item in data_dict.values():
+            print(item)
+            listOfData.append(item)
+        return  listOfData
+
 
 
 # {"firstName": "Levi", "lastName": "Gilbert", "email": "tesfhtin1278@gmail.com", "password": "abcd1234"} test
